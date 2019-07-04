4ae3e8e2623e2ee2147ee74303728e728e2b8442
Fabio Florentino <fmf.florentino@gmail.com>
2019-07-03 22:37:19 -0300

    Add Validation to Create Sessions

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,7 @@ import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.form.SessaoForm;
+import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
 
 @Controller
 public class SessaoController {
@@ -31,7 +34,7 @@ public class SessaoController {
 	
 	
 	@GetMapping("admin/sessao")
-	public ModelAndView forumarioDeCadastroDeSessao(@RequestParam("salaId") 
+	public ModelAndView formularioDeCadastroDeSessao(@RequestParam("salaId") 
 										Integer salaId, SessaoForm form ) {
 		
 		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
@@ -47,13 +50,20 @@ public class SessaoController {
 	@Transactional
 	public ModelAndView salva(@Valid SessaoForm form, BindingResult result) {
 		
-		if (result.hasErrors()) return forumarioDeCadastroDeSessao(form.getSalaId(),form);
+		if (result.hasErrors()) return formularioDeCadastroDeSessao(form.getSalaId(),form);
 		
 		Sessao sessao = form.toSessao(salaDao, filmeDao);
 		
-		sessaoDao.save(sessao);
+		List<Sessao> sessoesDaSala = sessaoDao.buscaSessoesDaSala(sessao.getSala());
 		
-		return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoesDaSala);
 		
+		if (gerenciador.cabe(sessao)) {
+			sessaoDao.save(sessao);
+			return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+		}
+		
+		return formularioDeCadastroDeSessao(form.getSalaId(), form);
+			
 	}
 }
