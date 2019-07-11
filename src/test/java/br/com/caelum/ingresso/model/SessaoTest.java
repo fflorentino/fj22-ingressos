1a476ca97341858bc6d76e4f851455d30608028b
Fabio Maran Florentino <fabio.florentino@platformbuilders.io>
2019-07-11 10:08:32 -0300

    add test to SessaoTest to check if a1 is ocupado, a2 and a3 is disponivel

--- src/test/java/br/com/caelum/ingresso/model/SessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/SessaoTest.java
@@ -3,24 +3,58 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import junit.framework.Assert;
 
 public class SessaoTest {
 	
+	private Filme rogueOne;
+	private Sala sala3D;
+	private Sessao sessao;
+	private Lugar a1;
+	private Lugar a2;
+	private Lugar a3;
+	
+	@Before
+	public void preparaSessoes() {
+		
+		this.rogueOne = new Filme("Rogue One", Duration.ofMinutes(120), "Ficção", new BigDecimal("12"));
+		this.sala3D = new Sala("Sala 3D", new BigDecimal("20.5"));
+		this.sessao = new Sessao(LocalTime.parse("10:00:00"), rogueOne, sala3D);
+		this.a1 = new Lugar("A", 1);
+		this.a2 = new Lugar("A", 2);
+		this.a3 = new Lugar("A", 3);
+	}
+	
 	@Test
 	public void oPrecoDaSesaoDeveSerIgualASomaDoPrecoDaSalaMaisOPrecoDoFile() {
-		Sala sala = new Sala("Eldorado - IMax", new BigDecimal(22.5));
-		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120),
-								"Ficção", new BigDecimal("12.0"));
+
 		
-		BigDecimal somaDosPrecosDaSalaEFilme = sala.getPreco().add(filme.getPreco());
-		
-		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		BigDecimal somaDosPrecosDaSalaEFilme = sala3D.getPreco().add(rogueOne.getPreco());
 		
 		Assert.assertEquals(somaDosPrecosDaSalaEFilme, sessao.getPreco());
+	}
+	
+	@Test
+	public void garanteQueOLugarA1estaOcupadoEOsLugaresA2EA3Disponivel() {
+		
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.INTEIRO, a1);
+		
+		Set<Ingresso> ingressos = Stream.of(ingresso).collect(Collectors.toSet());
+		
+		sessao.setIngressos(ingressos);
+		
+		Assert.assertFalse(sessao.isDisponivel(a1));
+		Assert.assertTrue(sessao.isDisponivel(a2));
+		Assert.assertTrue(sessao.isDisponivel(a3));
+		
+		
 	}
 
 }
